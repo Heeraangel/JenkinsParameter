45f14e9036ac53c1763534f15f493a8be7adff2c
HP <HP@HEERA>
2022-05-31 12:34:20 +0530

    parameter

---
+++ src/test/java/com/jenkinswithparameter/parameter/Parameter.java
@@ -0,0 +1,28 @@
+package com.jenkinswithparameter.parameter;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class Parameter {
+		@Test
+		public void parameterTest()
+		{
+			WebDriver driver = null;
+			String url=System.getProperty("url");
+			String browser=System.getProperty("browser");
+			switch(browser)
+			{
+				case "chrome":
+					driver=new ChromeDriver();
+					break;
+				case "edge":
+					driver=new EdgeDriver();
+					break;
+			}
+			driver.get(url);
+			System.out.println("Opened");
+			driver.quit();
+		}
+}
