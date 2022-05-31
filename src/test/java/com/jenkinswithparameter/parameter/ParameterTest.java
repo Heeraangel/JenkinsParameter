bd1eb178c0937bdc56c24709eb160e8641e05237
HP <HP@HEERA>
2022-05-31 13:20:05 +0530

    binary path added

--- src/test/java/com/jenkinswithparameter/parameter/ParameterTest.java
+++ src/test/java/com/jenkinswithparameter/parameter/ParameterTest.java
@@ -2,20 +2,22 @@ package com.jenkinswithparameter.parameter;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 public class ParameterTest {
 		@Test
 		public void parameterTest()
-		{
+		{ChromeOptions options = new ChromeOptions();
+		options.setBinary("C:\\Users\\HP\\AppData\\Local\\Google\\Chrome\\Application");
 			WebDriver driver = null;
 			String url=System.getProperty("url");
 			String browser=System.getProperty("browser");
 			switch(browser)
 			{
 				case "chrome":
-					driver=new ChromeDriver();
+					driver=new ChromeDriver(options);
 					break;
 				case "edge":
 					driver=new EdgeDriver();
