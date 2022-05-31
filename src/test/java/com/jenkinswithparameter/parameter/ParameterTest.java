08357d44cd0e5087b189c819fbe5f002f137018f
HP <HP@HEERA>
2022-05-31 13:28:41 +0530

    testcase updated

--- src/test/java/com/jenkinswithparameter/parameter/ParameterTest.java
+++ src/test/java/com/jenkinswithparameter/parameter/ParameterTest.java
@@ -9,15 +9,14 @@ import org.testng.annotations.Test;
 public class ParameterTest {
 		@Test
 		public void parameterTest()
-		{ChromeOptions options = new ChromeOptions();
-		options.setBinary("C:\\Users\\HP\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
+		{
 			WebDriver driver = null;
 			String url=System.getProperty("url");
 			String browser=System.getProperty("browser");
 			switch(browser)
 			{
 				case "chrome":
-					driver=new ChromeDriver(options);
+					driver=new ChromeDriver();
 					break;
 				case "edge":
 					driver=new EdgeDriver();
